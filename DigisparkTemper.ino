765d83fe0721dd74e575026bba550b42ab686cd5
Yoshihiko Miyaichi <miyaichi@gmail.com>
2019-05-24 09:18:01 +0900

    Timming control

--- DigisparkTemper.ino
+++ DigisparkTemper.ino
@@ -5,8 +5,11 @@
 #define DHTPIN 2
 DHT dht(DHTPIN, DHTTYPE);
 
+unsigned long last_millis;
+
 void setup()
 {
+  last_millis = millis();
   pinMode(1, OUTPUT);
   DigiUSB.begin();
   dht.begin();
@@ -14,21 +17,25 @@ void setup()
 
 void loop()
 {
-  // Wait a few seconds between measurements.
-  delay(2000);
+  // execute every 5 seconds
+  if (millis() - last_millis < 5000)
+    return;
+  last_millis = millis();
 
+  // measure temperature and humidity
   digitalWrite(1, HIGH);
   int8_t h = dht.readHumidity();
   int16_t t = dht.readTemperature(0);
   digitalWrite(1, LOW);
+  if ( t == BAD_TEMP || h == BAD_HUM )
+    return;
 
-  if ( t != BAD_TEMP && h != BAD_HUM ) {
-    DigiUSB.print(" ");
-    DigiUSB.print("{\"Temperature\": ");
-    DigiUSB.print(t);
-    DigiUSB.print(", \"Humidity\": ");
-    DigiUSB.print(h);
-    DigiUSB.println("}");
-    DigiUSB.delay(2000);
-  }
+  // report to usb
+  DigiUSB.print(" ");
+  DigiUSB.print("{\"Temperature\": ");
+  DigiUSB.print(t);
+  DigiUSB.print(", \"Humidity\": ");
+  DigiUSB.print(h);
+  DigiUSB.println("}");
+  DigiUSB.delay(2000);
 }
